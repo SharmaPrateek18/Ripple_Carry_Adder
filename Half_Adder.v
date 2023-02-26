fc271f7b5825cb8601bb3f5a8423be8666dd2d19
SharmaPrateek18 <126266854+SharmaPrateek18@users.noreply.github.com>
2023-02-27 01:54:18 +0530

    Update Half_Adder.v

--- Half_Adder.v
+++ Half_Adder.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 08:19:42 PM
-// Design Name: 
-// Module Name: Half_Adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module Half_Adder(a,b,s,cout);
